7300962b4a300daef2729051f470dd07b61df90b
elialbert <eli@simplerelevance.com>
2015-03-06 12:08:37 -0600

    even better colors

--- utils.js
+++ utils.js
@@ -107,10 +107,5 @@ function consoleLog(msg) {
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    return Math.random().toString(16).substr(-6);
 }
